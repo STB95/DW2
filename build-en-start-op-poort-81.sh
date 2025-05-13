1e09ff5963e0e8ef95b489d51c2bc4bba5854f7c
Sarah <168583870+STB95@users.noreply.github.com>
2025-05-13 12:16:22 +0200

    Update build-en-start-op-poort-81.sh

--- build-en-start-op-poort-81.sh
+++ build-en-start-op-poort-81.sh
@@ -2,9 +2,6 @@
 #image build met naam "demo-site-image"
 docker build -t demo-site-image .
 
-#moest de container al bestaan, wordt de vorige container eerst verwijderd:
-docker rm -f demo-site-container 2>/dev/null || true
-
 # Start nieuwe container (poort 81)
 docker run -dt -p 81:80 --name demo-site-container demo-site-image
 
