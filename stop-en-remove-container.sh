8424ceca6b97fe87572e8660202e9bcd67ce35f9
Sarah <168583870+STB95@users.noreply.github.com>
2025-05-13 11:49:41 +0200

    Update stop-en-remove-container.sh

--- stop-en-remove-container.sh
+++ stop-en-remove-container.sh
@@ -1,1 +1,6 @@
-Instructions
+# Container running? Stop en verwijder:
+docker stop demo-site-container 2>/dev/null || true
+docker rm demo-site-container 2>/dev/null || true
+
+echo "Container demo-site-container is gestopt en bijgevolg ook verwijderd."
+
