2cccaf2c735309ae7981d8f009a48386b631f8f6
Sarah <168583870+STB95@users.noreply.github.com>
2025-05-13 12:25:36 +0200

    Update build-en-start-op-poort-81.sh

--- build-en-start-op-poort-81.sh
+++ build-en-start-op-poort-81.sh
@@ -2,7 +2,7 @@
 #image build met naam "demo-site-image"
 docker build -t demo-site-image .
 
-# Start nieuwe container (poort 81)
-docker run -dt -p 81:80 --name demo-site-container demo-site-image
+# Start nieuwe container (poort 81) (-t is hier toch niet nodig? - container draait webserver, terminal dan toch niet nodig? )
+docker run -d -p 81:80 --name demo-site-container demo-site-image
 
 echo "Website vindbaar op http://localhost:81/"
