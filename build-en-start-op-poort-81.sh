508dd35a4ebe660863ece61fb070aef6d7876b93
Sarah <168583870+STB95@users.noreply.github.com>
2025-05-13 11:47:45 +0200

    Update build-en-start-op-poort-81.sh

--- build-en-start-op-poort-81.sh
+++ build-en-start-op-poort-81.sh
@@ -1,3 +1,11 @@
 ###Instructions
-docker build -t naam (achter p81-80)
-docker run -dt --name ... -p81-80 ....naam
+#image build met naam "demo-site-image"
+docker build -t demo-site-image .
+
+#moest de container al bestaan, wordt de vorige container eerst verwijderd:
+docker rm -f demo-site-container 2>/dev/null || true
+
+# Start nieuwe container (poort 81)
+docker run -dt -p 81:80 --name demo-site-container demo-site-image
+
+echo "Website vindbaar op http://localhost:81/"
